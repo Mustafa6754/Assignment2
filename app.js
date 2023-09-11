808e452398e05196192f9c212ca81e9955489846
Mustafa6754 <144300820+Mustafa6754@users.noreply.github.com>
2023-09-11 22:51:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+// Chapter 6
+x++;
+++x;
+x--;
+var x = 50;
+var y = x++;
+var y = 50;
+var z = --y;
+var newNum = --num;
+var newValue = oldVariable++;
+var num = 5;  
+num++;       
+alert(num);
+// Chapter 7
+var calculatedNum = 2 + (2 * 6);  14.
+var calculatedNum = 24.
+ calculatedNum = 24.
+calculatedNum = 18.
+var cost = (2 + (2 * 4) + 10);
+var units = (2 + 2) * 4 + 10;
+var pressure = (4 / 2) * 4;
+// Chapter 8
+"22"
+alert(message);
+"333"
+var part1 = "Pakistan";
+var part2 = "Zindabad";
+alert(part1 + " " + part2);
+var myString = "Hello, ";
+var myNumber = 42;
+var result = myString + myNumber;
+var string1 = "Hello, ";
+var string2 = "world!";
+var concatenatedString = string1 + string2;
+// Chapter 9
+var firstName = prompt("Enter first name");
+if (firstName !== null) {
+  alert("Hello, " + firstName + "!"); 
+} else {
+  alert("You did not enter a name.");
+}
+var yourName = prompt("Enter Your Name");
+var defaultValue = "John";
+var userInput = prompt("Please enter your name:", defaultValue);
+if (userInput !== null) {
+  alert("Hello, " + userInput + "!");
+} else {
+  alert("You canceled the prompt.");
+}
+var message = "Please enter your email:";
+var defaultResponse = "example@example.com";
+var userInput = prompt(message, defaultResponse);
+if (userInput !== null) {
+  alert("You entered: " + userInput);
+} else {
+  alert("You canceled the prompt.");
+}
+var message = "Please enter your favorite color:";
+var defaultResponse = "Blue";
+var userInput = prompt(message, defaultResponse);
+if (userInput !== null) {
+  alert("Your favorite color is: " + userInput);
+} else {
+  alert("You canceled the prompt.");
+}
+// Chapter 10
+var city = "Karachi";
+if (city === "Karachi") {
+  console.log("The City OF Lights");
+}
+if (x === y) {
+    var z = prompt("Please enter the value of z:"); 
+  }
+  var ZipCode = "10010";
+if (ZipCode === "10010") {
+  alert("Karachi");
+} else {
+  alert("Please write correct city");
+}
+var x = 1; 
+if (x === 1) { 
+  x = 2;
+}
+console.log(x); 
+// chapter 11
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
